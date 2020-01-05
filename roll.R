fccc288a11415c46ce742a947786838987eeeff4
utomoreza <utomorezadwi@gmail.com>
2020-01-05 17:12:04 +0700

    Editing roll() function and adding some descriptions about it.

--- roll.R
+++ roll.R
@@ -1,6 +1,10 @@
-roll <- function(ndie = 1) {
-  die <- 1:6
+# By using this function, you can roll die/dice with certain side(s).
+# First, you should define nside (number of sides of the die/dice) and ...
+# ndie (number of die/dice you'd like to roll).
+# Otherwise, both nside and ndie will be declared as 6 sides and 1 die respectively.
+# This function returns rolling results.
+
+roll <- function(nside = 6, ndie = 1) {
+  die <- 1:nside
   return(sample(die, size = ndie, replace = TRUE))
 }
-
-#roll(2)
